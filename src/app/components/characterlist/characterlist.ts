2bb76abe9c2c8b67a21eebb53a7b20ad9de989cf
ShadowK4t <saragmo1629@gmail.com>
2026-04-08 18:38:22 -0400

    Add characterlist and UI

--- src/app/components/characterlist/characterlist.ts
+++ src/app/components/characterlist/characterlist.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { HpService } from '../../services/hp';
+import { Character } from '../../models/character';
 
 @Component({
   selector: 'app-characterlist',
-  imports: [],
+  imports: [RouterLink, MatCardModule, MatProgressSpinnerModule],
   templateUrl: './characterlist.html',
-  styleUrl: './characterlist.css',
+  styleUrl: './characterlist.css'
 })
-export class Characterlist {}
+export class Characterlist implements OnInit {
+  characters = signal<Character[]>([]);
+  isLoading = signal<boolean>(true);
+  hasError = signal<boolean>(false);
+
+  constructor(private hpService: HpService) {}
+
+  ngOnInit(): void {
+    this.hpService.getAllCharacters().subscribe({
+      next: (data) => {
+        this.characters.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.hasError.set(true);
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
